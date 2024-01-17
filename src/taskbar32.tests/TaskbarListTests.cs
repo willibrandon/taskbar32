460af7fc4bec09aee7187392251aa0f1ec06bfc5
Brandon Williams <willibrandon@gmail.com>
2024-01-16 22:45:03 -0800

    Add taskbar tests and convert TaskbarList to CsWin32

--- src/taskbar32.tests/TaskbarListTests.cs
+++ src/taskbar32.tests/TaskbarListTests.cs
@@ -1,11 +1,117 @@
-namespace taskbar32.tests
-{
-    public class TaskbarListTests
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace taskbar32.tests;
 
-        }
+public class TaskbarListTests
+{
+    [Fact]
+    public void ActivateTab()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.ActivateTab();
+    }
+
+    [Fact]
+    public void DeleteTab_AddTab()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.DeleteTab();
+        taskbarHelper.AddTab();
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void MarkFullscreenWindow(bool fullScreen)
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.MarkFullscreenWindow(fullScreen);
+    }
+
+    [Fact]
+    public void SetActiveAlt()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetActiveAlt();
+    }
+
+    [Fact]
+    public void SetProgressState_Error()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetProgressState(TaskbarProgressState.Error);
+    }
+
+    [Fact]
+    public void SetProgressState_Indeterminate()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetProgressState(TaskbarProgressState.Indeterminate);
+    }
+
+    [Fact]
+    public void SetProgressState_NoProgress()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetProgressState(TaskbarProgressState.NoProgress);
+    }
+
+    [Fact]
+    public void SetProgressState_Normal()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetProgressState(TaskbarProgressState.Normal);
+    }
+
+    [Fact]
+    public void SetProgressState_Paused()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetProgressState(TaskbarProgressState.Paused);
+    }
+
+    [Fact]
+    public void SetProgressValue()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetProgressValue(50, 100);
+    }
+
+    [Fact]
+    public void SetThumbnailTooltip()
+    {
+        using Form form = new();
+        form.Show();
+
+        TaskbarHelper taskbarHelper = new(form.Handle);
+        taskbarHelper.SetThumbnailTooltip("Meow");
     }
 }
--- src/taskbar32/TaskbarList.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-namespace taskbar32;
-
-[ComImport]
-[Guid("56FDF344-FD6D-11d0-958A-006097C9A090")]
-[ClassInterface(ClassInterfaceType.None)]
-internal class TaskbarList { }
